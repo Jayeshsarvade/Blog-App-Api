1564d4bfcbabbb9041b1a3d608d339802d5db345
Jayeshsarvade <jayeshsarvade90@gmail.com>
2024-05-22 08:32:56 +0530

    commit user address main

--- user-address-service-blogappapi/src/main/java/com/blogappapi/useraddress/microservice/useraddressserviceblogappapi/UserAddress.java
+++ user-address-service-blogappapi/src/main/java/com/blogappapi/useraddress/microservice/useraddressserviceblogappapi/UserAddress.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class UserAddressServiceBlogappapiApplication {
+public class UserAddress{
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserAddressServiceBlogappapiApplication.class, args);
+		SpringApplication.run(UserAddress.class, args);
 	}
 
 }
